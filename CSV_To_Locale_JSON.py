71c3f28ff95a58a550ef5561bc72072f065bbd92
Sepand Yadollahifar <sepand_y@yahoo.com>
2022-08-31 19:11:49 +0430

    initial commit

---
+++ CSV_To_Locale_JSON.py
@@ -0,0 +1,42 @@
+import csv
+import json 
+
+# ask for file path in console
+file_path = input("Enter file path: ")
+
+# open file
+with open(file_path, 'r') as csv_file:
+	# read file
+	csv_reader = csv.reader(csv_file, delimiter=",", quotechar='"')
+	# create empty list
+	data = []
+	# loop through file
+	for row in csv_reader:
+		# add each row to list
+		data.append(row)
+
+# pop first row from list
+languages = data.pop(0)
+# print(data)
+# create empty dictionary
+# make an array of dictionaries per language
+locales = []
+for language in languages:
+	locales.append({})
+
+for row in data:
+	# loop from second item in row to last item in row
+	key = row[0]
+	for i in range(len(row)):
+		# if value is not empty
+		if row[i] != '':
+			# add key and value to dictionary
+			locales[i][key] = row[i]
+
+# write result in files
+for i in range(len(locales)):
+	with open(languages[i] + '.json', 'w') as json_file:
+		json_object = json.dumps(locales[i], indent = 4) 
+		json_file.write(json_object)
+		json_file.close()
+		print("File " + languages[i] + '.json' + " created.")
